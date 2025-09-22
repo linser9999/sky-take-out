47835c70bce3464027cf856635ccb730a197914f
linser <1811535824@qq.com>
2025-09-22 15:35:17 +0800

    c端完成用户下单业务代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -0,0 +1,108 @@
+package com.sky.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.AddressBook;
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import com.sky.entity.ShoppingCart;
+import com.sky.exception.AddressBookBusinessException;
+import com.sky.exception.ShoppingCartBusinessException;
+import com.sky.mapper.AddressBookMapper;
+import com.sky.mapper.OrderDetailMapper;
+import com.sky.mapper.OrderMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private AddressBookMapper addressBookMapper;
+    @Autowired
+    private OrderMapper orderMapper;
+    @Autowired
+    private OrderDetailMapper orderDetailMapper;
+
+
+    /**
+     * 用户下单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @Override
+    @Transactional
+    public Result<OrderSubmitVO> submit(OrdersSubmitDTO ordersSubmitDTO) {
+
+        // 1.查询相关地址簿
+        AddressBook addressBook = addressBookMapper.getById(ordersSubmitDTO.getAddressBookId());
+        // 该用户地址不存在，抛出异常
+        if (BeanUtil.isEmpty(addressBook)) {
+            throw new AddressBookBusinessException(MessageConstant.ADDRESS_BOOK_IS_NULL);
+        }
+
+        // 2.查询购物车信息
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUserId(userId);
+        List<ShoppingCart> list = shoppingCartMapper.select(shoppingCart);
+        // 购物车数据不存在，返回错误
+        if (list.isEmpty()) {
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+
+        // 3.处理orders订单数据
+        Orders orders = BeanUtil.copyProperties(ordersSubmitDTO, Orders.class);
+        orders.setNumber(String.valueOf(System.currentTimeMillis()));
+        orders.setStatus(Orders.PENDING_PAYMENT);
+        orders.setUserId(userId);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setPayStatus(Orders.UN_PAID);
+        orders.setPhone(addressBook.getPhone());
+        orders.setAddress(addressBook.getDetail());
+        orders.setConsignee(addressBook.getConsignee());
+
+        // 4.保存orders数据
+        orderMapper.save(orders);
+
+        // 5.处理order_detail数据
+        List<OrderDetail> orderDetails = new ArrayList<>();
+        // 将购物车数据拷贝到订单详情列表
+        for (ShoppingCart cart : list) {
+            OrderDetail orderDetail = new OrderDetail();
+            BeanUtil.copyProperties(cart, orderDetail);
+            orderDetail.setOrderId(orders.getId()); // 设置订单ID
+            orderDetails.add(orderDetail);
+        }
+
+        // 6.保存order_detail信息
+        orderDetailMapper.insertBatch(orderDetails);
+
+        // 7.清空购物车
+        shoppingCartMapper.clean(userId);
+
+        // 8.包装返回数据OrderSubmitVO
+        OrderSubmitVO orderSubmitVO = BeanUtil.copyProperties(orders, OrderSubmitVO.class);
+        orderSubmitVO.setOrderNumber(orders.getNumber());
+        orderSubmitVO.setOrderAmount(orders.getAmount());
+
+        // 9.返回数据
+        return Result.success(orderSubmitVO);
+    }
+}
